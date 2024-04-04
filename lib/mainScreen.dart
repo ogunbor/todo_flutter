b08f5be00f1cbec498fc6124b55aa645069fd0df
mompei <ogunborjerry@gmail.com>
2024-04-04 18:23:17 +0100

    Add Icons, scaffold actions, drawer

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -11,9 +11,18 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Text('Hello world'),
+      ),
       appBar: AppBar(
         centerTitle: true,
         title: Text('To do'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
